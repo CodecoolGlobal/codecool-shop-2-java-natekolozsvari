6d8f1e9e527fc309e74fd589b468d3b237baa780
Taowathar <taowathar@gmail.com>
2021-06-21 12:22:41 +0200

    Add price field to Order class

--- src/main/java/com/codecool/shop/service/Order.java
+++ src/main/java/com/codecool/shop/service/Order.java
@@ -2,24 +2,29 @@ package com.codecool.shop.service;
 
 import com.codecool.shop.model.Product;
 
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class Order {
     private Map<Product, Integer> shoppingCart;
+    private float price;
 
     public Order() {
         shoppingCart = new HashMap<>();
+        price = 0;
     }
 
     public void addToCart(Product product, int quantity) {
         shoppingCart.put(product, quantity);
+        price += product.getDefaultPrice() * quantity;
     }
 
     public void increaseQuantity(Product product) {
         if (shoppingCart.containsKey(product)) {
             int currentValue = shoppingCart.get(product);
             shoppingCart.put(product, currentValue + 1);
+            price += product.getDefaultPrice();
         }
     }
 
@@ -27,10 +32,15 @@ public class Order {
         if (shoppingCart.containsKey(product)) {
             int currentValue = shoppingCart.get(product);
             shoppingCart.put(product, currentValue - 1);
+            price -= product.getDefaultPrice();
         }
     }
 
     public Map<Product, Integer> getShoppingCart() {
         return shoppingCart;
+    }
+
+    public float getPrice() {
+        return price;
     }
 }
