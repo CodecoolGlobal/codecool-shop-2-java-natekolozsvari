b57741114ecbf7a90bf77d101592423bdf9f3649
Taowathar <taowathar@gmail.com>
2021-06-21 13:42:30 +0200

    Add eventListener to addToCart buttons

---
+++ src/main/webapp/static/js/main.js
@@ -0,0 +1,8 @@
+window.onload = () => {
+    let addToCartButtons = document.querySelectorAll(".btn-success");
+    for (let button of addToCartButtons) {
+        button.addEventListener('click', () => {
+
+        });
+    }
+}
