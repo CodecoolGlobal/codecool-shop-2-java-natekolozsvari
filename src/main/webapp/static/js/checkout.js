75e4f60b6b2085b226c13e8abfecc6dc000a1bb3
Taowathar <taowathar@gmail.com>
2021-06-23 15:46:49 +0200

    Add checkout.js

---
+++ src/main/webapp/static/js/checkout.js
@@ -0,0 +1,6 @@
+window.onload = () => {
+    let checkout = document.querySelector(".pay")
+    checkout.onclick = function () {
+        fetch(`/updateCart?name=clear`)
+            .then((response) => response.json())
+}}
