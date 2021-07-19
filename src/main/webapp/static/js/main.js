0b66faf18893b699e5155af172b1e27b7aaee992
natekolozsvari <nate.kolozsvari@gmail.com>
2021-07-19 12:02:50 +0200

    Create login modal

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -25,6 +25,7 @@ window.onload = () => {
     initSideBar();
     // initCartEventListener();
     initSignUpModal();
+    initLogInModal();
     cartHoverListener();
 }
 
@@ -327,6 +328,13 @@ function initSignUpModal() {
         }
     }
 
+    let alreadyReg = document.getElementsByClassName("already-reg")[0];
+    alreadyReg.onclick = function () {
+        modal.style.display = "none";
+        let logInModal = document.getElementById("logInModal");
+        logInModal.style.display = "block";
+    }
+
 // When the user clicks on <span> (x), close the modal
     span.onclick = function () {
         modal.style.display = "none";
@@ -340,4 +348,48 @@ function initSignUpModal() {
     }
 }
 
+function initLogInModal() {
+// Get the modal
+    var modal = document.getElementById("logInModal");
 
+// Get the button that opens the modal
+    var btn = document.getElementById("login-btn");
+
+// Get the <span> element that closes the modal
+    var span = modal.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+    btn.onclick = function () {
+        let modals = document.getElementsByClassName("modal");
+
+        Array.from(modals).forEach(function(m) {
+            if (m !== modal && m.style.display === "block"){
+                m.style.display = "none";
+            }
+        })
+        if (modal.style.display === "block") {
+            modal.style.display = "none";
+        } else {
+            modal.style.display = "block";
+        }
+    }
+
+    let noAcc = document.getElementsByClassName("no-acc")[0];
+    noAcc.onclick = function () {
+        modal.style.display = "none";
+        let signUpModal = document.getElementById("signUpModal");
+        signUpModal.style.display = "block";
+    }
+
+// When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+// When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    }
+}
