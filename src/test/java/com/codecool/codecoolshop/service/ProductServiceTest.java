8f08695873aeac48a80c5e65cbb235db317d20ff
Taowathar <taowathar@gmail.com>
2021-07-20 12:02:10 +0200

    Add getProductByName method tests

--- src/test/java/com/codecool/codecoolshop/service/ProductServiceTest.java
+++ src/test/java/com/codecool/codecoolshop/service/ProductServiceTest.java
@@ -1,6 +1,48 @@
 package com.codecool.codecoolshop.service;
 
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
+import com.codecool.shop.service.ProductService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+
 public class ProductServiceTest {
+    private ProductService productService;
+    private ProductDao productDao;
+    private ProductCategoryDao productCategoryDao;
+    private SupplierDao supplierDao;
+
+    @BeforeEach
+    void setup() {
+        productDao = mock(ProductDao.class);
+        productCategoryDao = mock(ProductCategoryDao.class);
+        supplierDao = mock(SupplierDao.class);
+        productService = new ProductService(productDao, productCategoryDao, supplierDao);
+    }
+
+    @Test
+    void getProductByName_ProductIsExists_returnTrue() {
+        ProductCategory toy = new ProductCategory("Toy", "Toy maker", "Toys for kids and adults only");
+        Supplier supplier = new Supplier("SouthPark Toys Inc.", "Creator of the best seller toys");
+        Product product = new Product("STOP touching me ELMO", 29.97f, "USD", "Elmo says inappropriate catchphrases, the doll also has a toothpaste dispenser", toy, supplier);
+        when(productDao.getByName("STOP touching me ELMO")).thenReturn(product);
+        Assertions.assertEquals(product, productService.getProductByName("STOP touching me ELMO"));
+    }
+
+    @Test
+    void getProductByName_ProductIsNotExists_returnNull() {
+        when(productDao.getByName("STOP touching me ELMO")).thenReturn(null);
+        assertNull(productService.getProductByName("STOP touching me ELMO"));
+    }
+
+
 }
