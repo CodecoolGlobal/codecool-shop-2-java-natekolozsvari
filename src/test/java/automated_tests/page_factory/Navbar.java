ae70f9a9852013a81ff1065f6689c26b04228bd1
CsabaToth70 <lesitocsa@gmail.com>
2021-07-21 16:00:18 +0200

    test: add locators to Navbar

--- src/test/java/automated_tests/page_factory/Navbar.java
+++ src/test/java/automated_tests/page_factory/Navbar.java
@@ -1,2 +1,31 @@
-package automated_tests.page_factory;public class Navbar {
+package automated_tests.page_factory;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Navbar {
+    WebDriver driver;
+
+    public Navbar(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//a[contains(text(),'Home')]")
+    WebElement homeBtn;
+
+    @FindBy(id = "filter-btn")
+    WebElement filterBtn;
+
+    @FindBy(css = ".shopcart")
+    WebElement cartBtn;
+
+    @FindBy(id = "login-btn")
+    WebElement loginBtn;
+
+    @FindBy(id = "signup-btn")
+    WebElement signUpBtn;
+
 }
