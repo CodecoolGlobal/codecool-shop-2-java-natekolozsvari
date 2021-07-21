cbe1912b80127ce6265661cf9c5d686a1b842fa2
CsabaToth70 <lesitocsa@gmail.com>
2021-07-21 14:49:12 +0200

    test: add starter elements for automated testing

---
+++ src/test/java/automated_tests/page_factory/Home.java
@@ -0,0 +1,18 @@
+package automated_tests.page_factory;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Home {
+    WebDriver driver;
+
+    public Home(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+
+
+}
