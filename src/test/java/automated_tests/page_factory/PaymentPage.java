810317165e73e1e9a0892d79e0b872fa95ce0f11
palerdos <palerdos95@gmail.com>
2021-07-22 11:13:56 +0200

    test: test created for successful checkout

--- src/test/java/automated_tests/page_factory/PaymentPage.java
+++ src/test/java/automated_tests/page_factory/PaymentPage.java
@@ -5,12 +5,24 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class PaymentPage {
     WebDriver driver;
 
+    WebDriverWait webDriverWait;
+
+    @FindBy(xpath = "//h3[contains(text(),'Payment')]")
+    WebElement paymentHeader;
+
     public PaymentPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(new AjaxElementLocatorFactory(driver, 5), this);
+    }
+
+    public boolean paymentHeaderIsDisplayed() {
+        return paymentHeader.isDisplayed();
     }
 }
--- src/test/java/automated_tests/test/TestCheckout.java
+++ src/test/java/automated_tests/test/TestCheckout.java
@@ -1,8 +1,41 @@
 package automated_tests.test;
 
+import automated_tests.page_factory.CheckoutPage;
+import automated_tests.page_factory.PaymentPage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestCheckout {
 
+    WebDriver driver = new ChromeDriver();
+    CheckoutPage checkoutPage;
+    PaymentPage paymentPage;
 
+    @BeforeEach
+    void setUp() {
+        TestingHelper.setUp(driver, "http://localhost:8888/checkout");
+        checkoutPage = new CheckoutPage(driver);
+        paymentPage = new PaymentPage(driver);
+    }
 
+    @AfterEach
+    void tearDown() {
+        driver.close();
+        driver.quit();
+    }
+
+    @ParameterizedTest()
+    @CsvFileSource(resources = "/successfulCheckoutData.csv", numLinesToSkip = 1)
+    void testSuccessfulCheckoutRedirectsToPaymentPage(String name, String email, String phoneNumber,
+                                                      String country, String address, String city, String zip) {
+        checkoutPage.fillBillingAddressForm(name, email, phoneNumber, country, address, city, zip);
+        checkoutPage.clickPayWithCreditCard();
+        assertTrue(paymentPage.paymentHeaderIsDisplayed());
+    }
 }
 
